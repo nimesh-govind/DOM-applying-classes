6377480717be2de29f1facb9ac59ca16c509e509
nimesh-govind <nimeshgovind74@gmail.com>
2020-07-14 21:05:23 +1200

    DOM Applying Classes complete excercise

--- classes.js
+++ classes.js
@@ -5,21 +5,41 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+   one()
     
   // Your turn! Create a new function called `two`, then call it from here.
-}
+  two()
+
+  three()
+
+  makeVisible()
+} 
 
 function one () {
   // First, we have to find the element:
   var one = document.getElementById('one')
+
+
 
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+var two = document.getElementById('two');
+two.classList.add('green');
+}
 
 // CREATE FUNCTION three HERE
-
+function three (){
+  var three = document.getElementById('three');
+  three.classList.add('yellow');
+}
 // CREATE FUNCTION makeVisible HERE
+function makeVisible (){
+  var invisible = document.getElementsByClassName('invisible')[0];
+  invisible.classList.add('visible');
+}
+
+
